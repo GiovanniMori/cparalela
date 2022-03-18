a22a472484dcf160bd1abc9ac998756c519a838a
Giovanni <giovanni_mori@hotmail.com>
2022-03-17 23:45:11 -0300

    struct

---
+++ lab03/struct.cpp
@@ -0,0 +1,3 @@
+// Faça o exercício: http://www.inf.pucrs.br/~pinho/LaproI/ExerciciosValendoNota2008-1/Exercicio-Struct.html
+//Siga a documentação de acordo com a linguagem de sua preferência, C ou C++.Observe que os links de documentação estão em C++
+//, mas isso não significa que o problema não possa ser resolvido em C.
